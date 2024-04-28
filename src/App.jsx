00bc2b2f0a27b79860319abf5ef4b2d0024114ef
loner2424 <sahilsobhani4@gmail.com>
2024-04-28 23:05:46 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import Overview from "./sections/Overview";
+import Services from "./sections/Services";
+import Clients from "./sections/Clients";
+import Presence from "./sections/Presence";
+import Awards from "./sections/Awards";
+import GetInTouch from "./sections/GetInTouch";
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Overview />
+      <Services />
+      <Clients />
+      <Presence />
+      <Awards />
+      <GetInTouch />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
